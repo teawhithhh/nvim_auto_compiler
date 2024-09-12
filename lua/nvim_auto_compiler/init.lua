83ab2dbaf17d558a8d6a38f2816a19d1f68699e5
chaenoed <125253179+teawhithhh@users.noreply.github.com>
2024-09-12 16:29:28 +0200

    Update init.lua

--- lua/nvim_auto_compiler/init.lua
+++ lua/nvim_auto_compiler/init.lua
@@ -16,7 +16,7 @@ function CompileCxx(cxx_command_template, cxx_flags, build_bat, dir_to_build_fil
 
 	if exist_file(output_dir, build_bat) == '0' then
 		print("Компиляция прошла успешно.\n")
-    		vim.fn.system('wt new-tab cmd /k "cd /d "'.. output_dir ..'" && build.bat && echo. && pause"')
+    		vim.fn.system('wt new-tab cmd /k "cd /d "'.. output_dir ..'" && build.bat && echo. && pause && exit"')
 		return;
 	end
 
@@ -29,7 +29,7 @@ function CompileCxx(cxx_command_template, cxx_flags, build_bat, dir_to_build_fil
   if status == '' then
     print("Компиляция прошла успешно.\n")
     print(start_cmd)
-    vim.fn.system('wt new-tab cmd /c cd "C:\\Users\\240821"' .. start_cmd .. ' && echo. && pause && exit"')
+    vim.fn.system('wt new-tab cmd /k "cd /d "C:\\Users\\240821" ' .. start_cmd .. ' && echo. && pause && exit"')
 	else
 		print("Ошибка компиляции. Код ошибки:\n" .. status)
 	end
